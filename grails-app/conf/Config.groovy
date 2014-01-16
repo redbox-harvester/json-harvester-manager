00da2e527f15212050913e58f298eccfa260c6e7
Shilo Banihit <shilo@redboxresearchdata.com.au>
2014-01-16 10:19:16 +1000

    REDBOX-187 #comment Adding Grails config for global rolling file appender.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -114,27 +114,33 @@ environments {
         // TODO: grails.serverURL = "http://www.changeme.com"
     }
 }
-
 // log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+	// Example of changing the log pattern for the default console appender:
+	//
+	//appenders {
+	//    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+	//}
 	
-	debug 'au.com.redboxresearchdata',
-		  'org.springframework.integration'
+	appenders {
+		rollingFile name: "mainRollingFileAppender", maxFileSize:1048576, maxFileIndex:10, file:"${config.userHome}/.grails/${config.appName}/logs/main.log"
+		console name:"console"
+	}
+	
+	debug console: ['au.com.redboxresearchdata',
+		  'org.springframework.integration'],
+		  mainRollingFileAppender: ['au.com.redboxresearchdata',
+		  'org.springframework.integration'] 
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+	error 'org.codehaus.groovy.grails.web.servlet',        // controllers
+		   'org.codehaus.groovy.grails.web.pages',          // GSP
+		   'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+		   'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+		   'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+		   'org.codehaus.groovy.grails.commons',            // core / classloading
+		   'org.codehaus.groovy.grails.plugins',            // plugins
+		   'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+		   'org.springframework',
+		   'org.hibernate',
+		   'net.sf.ehcache.hibernate'
 }
